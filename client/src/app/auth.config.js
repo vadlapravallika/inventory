adc3c0c435159d3c8fd907dad4e52608e83126b2
Pravalli <145421563+vadlapravallika@users.noreply.github.com>
2024-05-02 19:49:14 -0500

    Inventory - Undo

--- client/src/app/auth.config.js
+++ client/src/app/auth.config.js
@@ -8,8 +8,7 @@ export const authConfig = {
       authorized({ auth, request }) {
         const isLoggedIn = auth?.user;
         const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-        const isLoad = request.nextUrl.pathname.startsWith("/");
-        if (isOnDashboard || isLoad) {
+        if (isOnDashboard) {
           if (isLoggedIn) return true;
           return false;
         } else if (isLoggedIn) {
